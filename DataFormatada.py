4a2555a7774c75bcd3da0241d8477800860ef687
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2023-02-22 17:28:26 -0300

    v1.0
    Criação do pacote "DataFormatada" e alguma de suas funções

---
+++ DataFormatada.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+def data_hora():
+    data = str(datetime.now())
+    return f'{data[8:10]}/{data[5:7]}/{data[0:4]} {data[11:16]}'
+
+def data():
+    data = str(datetime.now())
+    return f'{data[8:10]}/{data[5:7]}/{data[0:4]}'
+
+def dia():
+    data = str(datetime.now())
+    return f'{data[8:10]}'
+
+def mes():
+    data = str(datetime.now())
+    return f'{data[5:7]}'
+
+def ano():
+    data = str(datetime.now())
+    return f'{data[0:4]}'
+
+def nome_mes():
+    data = str(datetime.now())
+    mes = data[5:7]
+
+    if mes == '01':
+        return 'Janeiro'
+    elif mes == '02':
+        return 'Fevereiro'
+    elif mes == '03':
+        return 'Março'
+    elif mes == '04':
+        return 'Abril'
+    elif mes == '05':
+        return 'Maio'
+    elif mes == '06':
+        return 'Junho'
+    elif mes == '07':
+        return 'Julho'
+    elif mes == '08':
+        return 'Agosto'
+    elif mes == '09':
+        return 'Setembro'
+    elif mes == '10':
+        return 'Outubro'
+    elif mes == '11':
+        return 'Novembro'
+    else:
+        return 'Dezembro'
+
+def eh_bissexto(ano):
+    ano = int(ano)
+    if ano % 4 == 0:
+        return True
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    #print(f'{dia()}/{mes()}/{ano()}')
+    #print(nome_mes())
+    eh_bissexto(2020)
